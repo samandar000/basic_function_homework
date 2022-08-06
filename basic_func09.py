095a4a6c3bf26f0e2e47037c1d942798746cec09
Samandar <samandarnfh.2020@gmail.com>
2022-08-06 16:13:28 +0500

    09

--- basic_func09.py
+++ basic_func09.py
@@ -1,5 +1,12 @@
 # Create a function called main.
 
 # Create function arguments a.
-
+  
 # decrease the value of a to one and return
+   
+def main():
+
+    a=5
+
+    return a-1,a
+print(main()) 
